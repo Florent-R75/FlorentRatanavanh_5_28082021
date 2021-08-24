c689bab997bdd36bfd3ff8ea7573bdcc413c2cb3
Florent-R75 <81759557+Florent-R75@users.noreply.github.com>
2021-08-24 09:32:18 +0200

    Galerie Image

--- public/js/index.js
+++ public/js/index.js
@@ -8,32 +8,64 @@ fetch("http://localhost:3000/api/teddies")
     }
   })
   .then(function (products) {
-    console.log(products);
     for (index = 0; index < products.length; index++) {
       showProduct(products[index]);
       console.log(products[index]);
     }
   })
+
   .catch(function (err) {
     console.log("Fetch problem" + err.message);
   });
 
 //   creer un variable source noeud HTML
 
-let root = document.querySelector("col-12 col-sm-4");
-
 // Affiche un produit a l'interieur du <root> element>
 function showProduct(product) {
-  console.log(product.imageUrl);
-  const section = document.createElement("section");
+  // Initialisation de variables
+
+  const card = document.createElement("a");
+  const image = document.createElement("img");
+  const cardbody = document.createElement("div");
+
   const heading = document.createElement("h2");
   const para = document.createElement("p");
-  const image = document.createElement("img");
+  const description = document.createElement("p");
+  const colorsList = document.createElement("ul");
+  const colors = document.createElement("li");
 
-  section.setAttribute("class", product.type);
+  // Attribution de contenus
 
-  /* main.appenChild(section);
-  section.appenChild(heading);
-  section.appenChild(para);
-  section.appenChild(image);*/
+  heading.textContent = product.name;
+  para.textContent = product.price + "€";
+  colors.textContent = product.colors;
+
+  // Attribution de classes
+
+  image.setAttribute("src", product.imageUrl);
+  image.setAttribute("alt", product.description);
+  card.setAttribute("class", "col-12 col-sm-4");
+
+  // Attribution de noeuds NOD
+
+  let root = document.querySelector(".teddy");
+  // let colorsArray = [];
+  root.appendChild(card);
+
+  card.appendChild(image);
+  card.appendChild(cardbody);
+  cardbody.appendChild(heading);
+  cardbody.appendChild(para);
+  cardbody.appendChild(colorsList);
+  colorsList.appendChild(colors);
+
+  // Attribution Couleurs
+
+  // for (index = 0; index < colorsArray.length; i++) {
+  // colors = colorsArray[index];
+  // }
+
+  // style css
+
+  image.style.height = "300px";
 }
